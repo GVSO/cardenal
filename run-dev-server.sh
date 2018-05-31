a81ee411730fd524f0a7d747e5331a8844682b1f
Getulio Sánchez <valentin2507@gmail.com>
2018-05-31 19:11:42 -0400

    Kill processes when shell is killed

--- run-dev-server.sh
+++ run-dev-server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Run Go server application.
-go run settings.go handlers.go linkedin.go main.go &
+go run settings.go handlers.go linkedin.go main.go | sed "s/^/[Go Server] /" &
 
 # Run React application.
 cd client
-npm start &
+npm start | sed "s/^/[React] /"  &
+
+wait
